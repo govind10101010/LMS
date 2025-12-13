ff48acc9a002b473deb4c30049b72e3c2dd4f620
Govind <govindjha1010@gmail.com>
2025-12-13 23:09:53 +0530

    stripe_added

--- Server/server.js
+++ Server/server.js
@@ -1,29 +1,42 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
-import connectDB from './configs/mongodb.js'
-import { clerkWebhooks } from './controllers/webhooks.js'
+import connectDB from './configs/mongodb.js';
+import { clerkWebhooks, stripeWebhooks } from './controllers/webhooks.js';
+import educatorRouter from './routes/educatorRoutes.js';
+import { clerkMiddleware } from '@clerk/express';
+import connectCloudinary from './configs/cloudinary.js';
+import courseRouter from './routes/courseRoute.js';
+import userRouter from './routes/userRoutes.js';
+
+// initialize express 
+const app = express();
 
 
-// Initialize express
-const app = express()
+// connect to db
+await connectDB();
+await connectCloudinary();
 
-// Middlewares
-app.use(cors())
-app.use(express.json())  // required for JSON body parsing
 
-// Connect to database
-await connectDB()
+// middleware
+app.use(cors());
+app.use(clerkMiddleware())
+
 
 // Routes
-app.get('/', (req, res) => res.send("API Working"))
-
-
+app.get('/', (req,res)=>{res.send("Edemy API is working fine!")})
 app.post('/clerk', express.json(), clerkWebhooks)
+app.use('/api/educator', express.json(), educatorRouter);
+app.use('/api/course', express.json(), courseRouter);
+app.use('/api/user', express.json(), userRouter);
+app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks);
 
-// Port
-const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
+
+// port
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=> {
+    console.log(`Server is running on ${PORT}`);
+    
 })
